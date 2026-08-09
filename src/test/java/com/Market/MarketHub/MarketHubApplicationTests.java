42d2610f2e3f2d8acfb88a6e094376b69a3da693
Ihsan Afridi <ihsanofd@gmail.com>
2026-08-09 09:43:07 +0500

    Initial commit

---
+++ src/test/java/com/Market/MarketHub/MarketHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Market.MarketHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
